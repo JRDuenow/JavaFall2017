648a1632fcf5451d6734858a61f8583ae68ba1cf
Justin D <firebolt0777@gmail.com>
2017-09-12 15:52:15 -0500

    Changes

--- p2JustinD/src/GameWindow.java
+++ p2JustinD/src/GameWindow.java
@@ -1,5 +1,6 @@
 
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,9 +8,18 @@ import javax.swing.*;
 
 public class GameWindow extends JFrame {
 
+    enum State {
+        X, O
+    };
+
+    enum GameState {
+        Playing, Draw, X_Won, O_Won
+    }
+
+    State currentPlayer;
     JTextField textTopField;
     Container frameContainer;
-    JButton[] buttonArray;
+    GamePiece[] buttonArray;
 
     public GameWindow() {
         super("p2 Justin Duenow");
@@ -18,12 +28,14 @@ public class GameWindow extends JFrame {
         InitComponents();
         this.setLocationRelativeTo(null); //center the frame
         this.setVisible(true);
+        this.setResizable(false);
     }
 
     private void InitComponents() {
+        currentPlayer = State.O;
         frameContainer = this.getContentPane();
-        textTopField = new JTextField("Play Tic-Tac-Toe: O’s turn!");
-        buttonArray = new JButton[9];
+        textTopField = new JTextField(String.format("Play Tic-Tac-Toe: %s’s turn!", currentPlayer));
+        buttonArray = new GamePiece[9];
 
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
@@ -34,11 +46,14 @@ public class GameWindow extends JFrame {
         JPanel panelButtons = new JPanel();
         panelButtons.setLayout(new GridLayout(3, 3));
         ButtonListener listener = new ButtonListener();
+        Font btnFont = new Font("Courier New", Font.BOLD, 20);
         for (int i = 0; i < 9; i++) {
-            JButton tmpButton = buttonArray[i] = new JButton();
-            tmpButton.addActionListener(listener);
 
+            GamePiece tmpButton = buttonArray[i] = new GamePiece();
+            tmpButton.addActionListener(listener);
+            tmpButton.setFont(btnFont);
             panelButtons.add(tmpButton);
+
         }
 
         frameContainer.add(textTopField, "North");
@@ -49,14 +64,67 @@ public class GameWindow extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent ae) {
+            GamePiece tempPiece;
             for (int i = 0; i < buttonArray.length; i++) {
-                if (buttonArray[i] == ae.getSource()){
-                    System.out.println("Pressed at " + i);
-                    buttonArray[i].setEnabled(false);
+
+                if ((tempPiece = buttonArray[i]) == ae.getSource()) {
+                    tempPiece.setEnabled(false);
+                    tempPiece.setPieceState(currentPlayer);
+
+                    switch (checkBoardState()) {
+                        case Draw:
+                            textTopField.setText("Draw!");
+                            break;
+                        case X_Won:
+                            textTopField.setText("X Won!");
+                            break;
+                        case O_Won:
+                            textTopField.setText("O Won!");
+                            break;
+                        case Playing:
+                            currentPlayer = currentPlayer == State.O ? State.X : State.O;
+                            textTopField.setText(String.format("%s’s turn", currentPlayer));
+                            break;
+                    }
+
+                    break;
                 }
             }
+
         }
 
     }
 
+    private GameState checkBoardState() {
+        
+        return GameState.Playing;
+    }
+    
+    private boolean checkCells(GamePiece p1, GamePiece p2, GamePiece p3){
+        
+        if (p1.getPieceState() == null || p2.getPieceState() == null || p3.getPieceState() == null)
+            return false;
+        
+        
+        return p1.getPieceState().equals(p2.getPieceState()) && p2.getPieceState().equals(p3.getPieceState());
+    }
+
+    private class GamePiece extends JButton {
+
+        private State pieceState;
+
+        public GamePiece() {
+            pieceState = null;
+        }
+
+        public State getPieceState() {
+            return pieceState;
+        }
+
+        public void setPieceState(State pieceState) {
+            this.pieceState = pieceState;
+            this.setText(pieceState.name());
+        }
+
+    }
 }
