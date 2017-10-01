620c55b16222ffae61dbc916d5ff320f02103aca
Justin <Caedis@users.noreply.github.com>
2017-09-30 23:17:17 -0500

    Moved some of the window method calls to the main class

---
+++ p4JustinD/src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        GameWindow window = new GameWindow();
+        
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        
+    }
+}
